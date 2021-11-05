818adf97e59f761a374b79614ba142b288ea6e0e
kerlos1256 <kero24862@gmail.com>
2021-11-06 00:19:22 +0200

    updated db url

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -4,7 +4,7 @@ import { file } from './entities/image.entity';
 export const typeOrmConfig: PostgresConnectionOptions = {
   type: 'postgres',
   // host: 'ec2-34-226-18-183.compute-1.amazonaws.com',
-  url: 'postgres://lylhgqxjlyymdz:ac02afa0d350840cdee62854bcf15692775cb1af64eef5d58de4a0ad54173617@ec2-34-226-18-183.compute-1.amazonaws.com:5432/dflmkt5n2ckeck',
+  url: process.env.DATABASE_URL,
   // database: 'dflmkt5n2ckeck',
   // port: 5432,
   // username: 'lylhgqxjlyymdz',
